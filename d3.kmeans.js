c846fc546347f17753ba70cd2a44ab048c3d5a85
Yen-Chi Chen <gruenen@gmail.com>
2016-03-09 23:55:02 +0800

    generalise to n-dimentional kmeans

--- d3.kmeans.js
+++ d3.kmeans.js
@@ -6,13 +6,59 @@ function kmeans(arrayToProcess, numberOfClusters, accessor) {
     var changed = false;
 
     if (!accessor) {
-      accessor = function(d) { return d; };
+      accessor = function(d) { return [d]; };
     }
+
+    var dist = function (a, b) {
+      if (typeof a !== Array || typeof b !== Array) {
+        throw "Accessor has to return an array of values."
+      }
+      if (a.length !== b.length) {
+        throw "Number of dimensions not aligned between data points."
+      }
+
+      var ds = 0;
+      for (var i = 0; i < a.length; i++) {
+        ds += Math.pow(a[i] - b[i], 2);
+      }
+      return Math.sqrt(ds);
+    };
+
+    var calCentroid = function (group) {
+      var centroid = [];
+      var dim = group[0].length;
+      for (var i = 0; i < dim; i++) {
+        centroid.push(
+          group.map(function(p) { return p[i];})
+              .reduce(function (prevVal, currVal, currIdx) {
+                    return prevVal + currVal;
+                  }) / group.length );
+      }
+      return centroid;
+    };
+
+    var moveCentroids = function(groups) {
+      return groups.map(calCentroid);
+    };
+
+    var centroidsChanged = function(old, curr) {
+      var ret = false;
+      for (var i = 0; i < groups; i++) {
+        ret = ret ||
+        old.reduce(function(prevVal, currVal, j) {
+          return prevVal ||
+          currVal.reduce(function(prevVal, currVal, k){
+            return prevVal || currVal !== curr[j][k];
+          }, false);
+        }, false);
+      }
+      return ret;
+    };
 
     // initialise group arrays
     for (var initGroups = 0; initGroups < numberOfClusters; initGroups++ ) {
       groups[initGroups] = new Array();
-    }  
+    }
 
     // pick initial centroids
     var initialCentroids = Math.round(arrayToProcess.length / (numberOfClusters+1));
@@ -35,7 +81,7 @@ function kmeans(arrayToProcess, numberOfClusters, accessor) {
         var oldDistance = -1
         var newGroup;
         for (j=0; j < numberOfClusters; j++) {
-          distance = Math.abs(centroids[j] - accessor(arrayToProcess[i]));
+          distance = dist(centroids[j], accessor(arrayToProcess[i]));
           if (oldDistance == -1) {
             oldDistance = distance;
             newGroup = j;
@@ -48,22 +94,10 @@ function kmeans(arrayToProcess, numberOfClusters, accessor) {
       }
 
       oldCentroids = centroids;
-      for (j = 0 ; j < numberOfClusters; j++ ) {
-        var total = 0;
-        var newCentroid = 0;
-        for (i=0; i < groups[j].length; i++) {
-          total += accessor(groups[j][i]);
-        } 
-        newCentroid = total / groups[newGroup].length;  
-        centroids[j] = newCentroid;
-      }
-
-      for (j=0;j < numberOfClusters; j++ ) {
-        if (centroids[j] != oldCentroids[j]) {
-          changed = true;
-        }
-      }
+      centroids = moveCentroids(groups);
+      changed = centroidsChanged(oldCentroids, centroids);
     } while (changed == true);
+
     return groups;
 }
 if (typeof d3 !== "undefined") {
