ee2b5b23410b70053bf009ae5069fda8ba40b632
Yen-Chi Chen <gruenen@gmail.com>
2017-12-28 21:47:33 +0800

    multi-dimension, compatible with single dimension

--- d3.kmeans.js
+++ d3.kmeans.js
@@ -1,104 +1,117 @@
 'use strict';
 function kmeans(arrayToProcess, numberOfClusters, accessor) {
-    var groups = new Array();
-    var centroids = new Array();
-    var oldCentroids = new Array();
-    var changed = false;
+  var groups = new Array();
+  var centroids = new Array();
+  var oldCentroids = new Array();
+  var changed = false;
+  /*
+   * 1. no accessor provided
+   * 2. accessor returns a single value (1-dimensional)
+   * 3. accessor returns an array (multi-dimentioanl)
+   */
+  if (!accessor) {
+    accessor = function (d) { return [d]; };
+  }
 
-    if (!accessor) {
-      accessor = function(d) { return [d]; };
+  var dist = function (a, b) {
+    a = (typeof a === Array) ? a : [a];
+    b = (typeof b === Array) ? b : [b];
+    if (a.length !== b.length) {
+      throw "Number of dimensions not aligned between data points."
     }
 
-    var dist = function (a, b) {
-      if (typeof a !== Array || typeof b !== Array) {
-        throw "Accessor has to return an array of values."
-      }
-      if (a.length !== b.length) {
-        throw "Number of dimensions not aligned between data points."
-      }
+    var ds = 0;
+    for (var i = 0; i < a.length; i++) {
+      ds += Math.pow(a[i] - b[i], 2);
+    }
+    return Math.sqrt(ds);
+  };
 
-      var ds = 0;
-      for (var i = 0; i < a.length; i++) {
-        ds += Math.pow(a[i] - b[i], 2);
+  var calCentroid = function (group) {
+    var centroid = [];
+    var dim = group[0].length;
+    for (var i = 0; i < dim; i++) {
+      centroid.push(
+        group.map(function (p) { return p[i]; })
+          .reduce(function (prevVal, currVal) {
+            return prevVal + currVal;
+          }, 0) / group.length);
+    }
+    return centroid;
+  };
+
+  var moveCentroids = function (groups) {
+    return groups.map(calCentroid);
+  };
+
+  var identicalCoordinate = function(a, b) {
+    for (var d = 0; d <= a.length; d++) {
+      if (a[d] !== b[d]) return false;
+    }
+    return true;
+  };
+
+  var existsIn = function(basket, p) {
+    for(var j = 0; j <= basket.length; j++) {
+      if (identicalCoordinate(basket[j], p)) {
+        return true;
       }
-      return Math.sqrt(ds);
-    };
+    }
+    return false;
+  };
 
-    var calCentroid = function (group) {
-      var centroid = [];
-      var dim = group[0].length;
-      for (var i = 0; i < dim; i++) {
-        centroid.push(
-          group.map(function(p) { return p[i];})
-              .reduce(function (prevVal, currVal, currIdx) {
-                    return prevVal + currVal;
-                  }) / group.length );
-      }
-      return centroid;
-    };
+  var centroidsChanged = function (oldSet, currSet) {
+    // every old centroid should be found in new set, otherwise consider centroids changed
+    for (var i = 0;i <= oldSet.length; i++) {
+      if (!existsIn(currSet, oldSet[i])) return true;
+    }
+    return false;
+  };
 
-    var moveCentroids = function(groups) {
-      return groups.map(calCentroid);
-    };
+  // initialise group arrays
+  for (var initGroups = 0; initGroups < numberOfClusters; initGroups++) {
+    groups[initGroups] = new Array();
+  }
 
-    var centroidsChanged = function(old, curr) {
-      var ret = false;
-      for (var i = 0; i < groups; i++) {
-        ret = ret ||
-        old.reduce(function(prevVal, currVal, j) {
-          return prevVal ||
-          currVal.reduce(function(prevVal, currVal, k){
-            return prevVal || currVal !== curr[j][k];
-          }, false);
-        }, false);
-      }
-      return ret;
-    };
+  // hop for systemical sampling
+  var hop = Math.round(arrayToProcess.length / (numberOfClusters + 1));
+  var i, j, idx;
 
-    // initialise group arrays
-    for (var initGroups = 0; initGroups < numberOfClusters; initGroups++ ) {
-      groups[initGroups] = new Array();
+  for (i = 0; i < numberOfClusters; i++) {
+    idx = hop * (i + 1);
+    centroids[i] = accessor(arrayToProcess[idx]);
+  }
+
+  do {
+    for (j = 0; j < numberOfClusters; j++) {
+      groups[j] = [];
     }
 
-    // pick initial centroids
-    var initialCentroids = Math.round(arrayToProcess.length / (numberOfClusters+1));
-    var i, j, idx;
+    changed = false;
 
-    for (i=0; i < numberOfClusters; i++) {
-      idx = initialCentroids * (i+1);
-      centroids[i] = accessor(arrayToProcess[idx]);
-    }
-
-    do {
-      for( j=0; j < numberOfClusters; j++ ) {
-        groups[j] = [];
-      }
-
-      changed = false;
-
-      for (i=0; i < arrayToProcess.length; i++) {
-        var distance = -1;
-        var oldDistance = -1
-        var newGroup;
-        for (j=0; j < numberOfClusters; j++) {
-          distance = dist(centroids[j], accessor(arrayToProcess[i]));
-          if (oldDistance == -1) {
-            oldDistance = distance;
-            newGroup = j;
-          } else if (distance <= oldDistance) {
-            newGroup = j;
-            oldDistance = distance;
-          }
+    for (i = 0; i < arrayToProcess.length; i++) {
+      var distance = -1;
+      var oldDistance = -1
+      var newGroup;
+      for (j = 0; j < numberOfClusters; j++) {
+        distance = dist(centroids[j], accessor(arrayToProcess[i]));
+        if (oldDistance == -1) {
+          oldDistance = distance;
+          newGroup = j;
+        } else if (distance <= oldDistance) {
+          newGroup = j;
+          oldDistance = distance;
         }
-        groups[newGroup].push( arrayToProcess[i] );
       }
+      groups[newGroup].push(arrayToProcess[i]);
+    }
 
-      oldCentroids = centroids;
-      centroids = moveCentroids(groups);
-      changed = centroidsChanged(oldCentroids, centroids);
-    } while (changed == true);
+    oldCentroids = centroids;
+    centroids = moveCentroids(groups);
+    changed = centroidsChanged(oldCentroids, centroids);
+  } while (changed == true);
 
-    return groups;
+  return groups;
 }
 if (typeof d3 !== "undefined") {
   d3.kmeans = kmeans;
